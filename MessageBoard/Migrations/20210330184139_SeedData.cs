b5e02e1a117b82e3775af97197ef70d6fcff0355
Michael Pingel <mdpingel@gmail.com>
2021-03-30 13:46:37 -0700

    add seeded data
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

---
+++ MessageBoard/Migrations/20210330184139_SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Author", "Contents", "Date", "Title" },
+                values: new object[] { 1, "Bob", "What's up", "1/1/2021", "Yo" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Author", "Contents", "Date", "Title" },
+                values: new object[] { 2, "Jessica", "hello goodbye", "2/3/2021", "Whatever" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Author", "Contents", "Date", "Title" },
+                values: new object[] { 3, "Tim", "sdfdfsdfds", "3/4/2021", "Bye" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 3);
+        }
+    }
+}
--- MessageBoard/Models/MessageBoardContext.cs
+++ MessageBoard/Models/MessageBoardContext.cs
@@ -6,8 +6,19 @@ namespace MessageBoard.Models
   {
     public MessageBoardContext(DbContextOptions<MessageBoardContext> options): base(options)
     {
+
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Message>()
+      .HasData(
+        new Message { MessageId = 1, Title = "Yo", Author = "Bob", Date = "1/1/2021", Contents = "What's up"},
+        new Message { MessageId = 2, Title = "Whatever", Author = "Jessica", Date = "2/3/2021", Contents = "hello goodbye"},
+        new Message { MessageId = 3, Title = "Bye", Author = "Tim", Date = "3/4/2021", Contents = "sdfdfsdfds"}
+      );
+
+    }
     public DbSet<Message> Messages { get; set; }
   }
 }
